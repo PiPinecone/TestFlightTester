608a165062d5173c0643ce65343525c4afe9c07d
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2021-07-30 16:26:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
+# Definitions
+Bclosed = "This beta is full."
+
 # Yaml config loading
 with open('config.yaml') as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
@@ -14,8 +17,15 @@ opt = Options()
 opt.add_argument('--headless')
 driver = webdriver.Chrome(executable_path=data['DriverPath'], options=opt)
 
+
 # Determining whether the beta is open or closed
 driver.get(data['BetaURL'])
-elem = driver.find_element(By.XPATH, "//div[@class='beta-status']")
-print(elem.text)
+stat = driver.find_element(By.XPATH, "//div[@class='beta-status']").text
+
+if stat == Bclosed:
+    print("Beta full")
+    # Add more code to execute
+else:
+    print("Beta open")
+    # Add more code to execute
 driver.close()
